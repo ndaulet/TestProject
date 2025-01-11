eaa8a3ec4df1aee3bd24be69618ab0b100febbe9
ndaulet <ovelegia.o@gmail.ru>
2025-01-11 22:14:34 +0500

    homeWork_1

---
+++ HomeWork_1.js
@@ -0,0 +1,79 @@
+// Задание 4
+// let text = "my text"
+// if(text == "some text"){
+//     text = "another text"
+// }else{
+//     text = "some text"
+// }
+// Задание 5
+// let num = 0
+// if(num === 0){
+//     num = 1
+// }else if ( num < 0){
+//     num = "less than zero"
+// }else if( num < 0){
+//     num *= 10
+// }
+// Задание 6
+// let num = +prompt("введите число")
+// let result 
+// if(num < 5){
+//     result = "0"
+// }else if(num > 5){
+//     result = "1"
+// }
+// alert(result)
+// Задание 7
+// let num1 = prompt("введите число")
+// let num2 = prompt("введите второе число")
+// let biggest
+// if(num1 > num2){
+//     biggest = num1
+// }else if(num1 < num2){
+//     biggest = num2
+// }else if( num1 === num2){
+//     biggest = num1
+// }
+// Задание 8
+// comsole.log("большее число " +biggest)
+
+// let num1 = +prompt("введите число")
+// let num2 = +prompt("введите второе число")
+// if(num1 % num2 === 0){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// Задание 9
+// let mark = +prompt("введите средний балл")
+// if(mark >=1 && mark <= 4){
+//     console.log("Двоечник, иди учись")
+// }else if(mark >=5 && mark <= 8){
+//     console.log("Неплохо, но давай еще поднажмем")
+// }else if(mark >=9 && mark <= 10){
+//     console.log("Ого, да ты настоящий отличник")
+// }else{
+//     console.log("введите балл от 1 до 10")
+// }
+// Задание 10
+// let exam = +prompt("введите балл за экзамен")
+// let project = +prompt("количество выполненных проектов")
+// if(exam >= 90 && project >= 10){
+//     console.log("100")
+// }else if(exam >= 75 && project >= 5){
+//     console.log("90")
+// }else if(exam >= 50 && project >= 2){
+//     console.log("75")
+// }else{
+//     console.log("0")
+// }
+// Задание 11
+// let rent = +prompt("введите количество дней аренды")
+// if(rent >= 7){
+//     console.log("скидка 50$")
+// }else if( rent >=3){
+//     console.log("скидка 20$")
+// }else{
+//     console.log("скидки не буит(")
+// }
